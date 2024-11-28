b4808b904b9d6c877afb7d4443c8407eb7ef6a26
AlexZ-WP <alexz10@mail.ru>
2024-11-28 23:44:39 +0300

    Add files via upload

--- module_6_3.py
+++ module_6_3.py
@@ -1,52 +1,79 @@
-class Animal:
-    def __init__(self, speed):
-        self.speed = speed
+import random
 
-    _cords = []
+from pyglet.image.codecs.s3tc import decode_dxt3
+
+
+class Animal:
     live = True
     sound = None
     _DEGREE_OF_DANGER = 0
 
+    def __init__(self, speed):
+        self.speed = speed
+        self._cords = [0, 0, 0]
+
+
     def move(self, x, y, z):
         d = self.speed
-        self._cords[0] = d * x
-        self._cords[1] = d * y
-        self._cords[2] = d * z
+        dx = d*x
+        dy = d*y
+        dz = d*z
+        if dz < 0:
+            print("It's too deep, i can't dive :(")
+        else:
+           self._cords = [dx, dy, dz]
 
-    def get_cords(self, dx, dy, dz):
+    def get_cords(self):
         print(f"X: {self._cords[0]}, Y: {self._cords[1]} , Z: {self._cords[2]}")
 
     def attack(self):
-        if _DEGREE_OF_DANGER < 5:
+        if self._DEGREE_OF_DANGER < 5:
             print("Sorry, i'm peaceful :)")
-        elif _DEGREE_OF_DANGER >= 5:
+        elif self._DEGREE_OF_DANGER >= 5:
             print("Be careful, i'm attacking you 0_0")
         return
 
-
-class Bird(Animal):
+class  Bird(Animal):
     beak = True
 
-    def lay_eggs(self, egg):
-        print(f"Here are(is) {self.egg} eggs for you")
-
+    def lay_eggs(self):
+        # random.randint(1, 4) - вывод случайного числа от 1 до 4
+        print("Here are(is)",  random.randint(1, 4), "eggs for you")
 
 class AquaticAnimal(Animal):
-    super().__init__(speed)
+
 
     def dive_in(self, z):
-        abs(z) * (self.speed / 2)
 
+        self._cords[2] -=  int(abs(z) * self.speed/2) # self._cords[2] = dz, [а -= в] то же самое, что и [a = a-b],
+        # dz = z*d, d - скорость, уменьшаем dz на это значение
 
 class PoisonousAnimal(Animal):
     _DEGREE_OF_DANGER = 8
-    super().__init__(speed)
 
-
-class Duckbill(Animal, AquaticAnimal, Bird, PoisonousAnimal):
-    super().__init__(speed=1)
+class Duckbill(Bird, AquaticAnimal, PoisonousAnimal):
     sound = "Click-click-click"
-    super().dive_in(z)
+    def __init__(self, speed):
+        super().__init__(speed)
+
+    def speak(self):
+        print(self.sound)
 
 
-print(Duckbill.mro())
+db = Duckbill(10)
+
+print(db.live)
+print(db.beak)
+
+db.speak()
+db.attack()
+
+db.move(1, 2, 3)
+db.get_cords()
+db.dive_in(6)
+db.get_cords()
+
+
+db.lay_eggs()
+
+
